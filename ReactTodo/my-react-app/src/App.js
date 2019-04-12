4fbe1baf904306362b64ac8da07eaaf5cfc21936
Matt Harrison <fittocode@gmail.com>
2019-04-12 17:58:48 -0400

    working on state

---
+++ ReactTodo/my-react-app/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with Boss',
+        completed: false
+      }
+    ]
+  };
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
+}
+
+export default App;
