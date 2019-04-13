e6b81054526a0a32a9450de15325ee41ef6c2354
Matt Harrison <fittocode@gmail.com>
2019-04-12 20:28:14 -0400

    added some more

--- ReactTodo/my-react-app/src/App.js
+++ ReactTodo/my-react-app/src/App.js
@@ -1,31 +1,62 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 class App extends Component {
   state = {
     todos: [
       {
         id: 1,
-        title: 'Take out the trash',
+        title: 'Finish React App',
         completed: false
       },
       {
         id: 2,
-        title: 'Dinner with wife',
+        title: 'Clean my Room',
         completed: false
       },
       {
         id: 3,
-        title: 'Meeting with Boss',
+        title: 'Fold Laundry',
         completed: false
       }
     ]
   };
+
+  // toggle complete
+
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // Delete Todo
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+
   render() {
     console.log(this.state.todos);
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
--- ReactTodo/my-react-app/src/components/TodoItem.js
+++ ReactTodo/my-react-app/src/components/TodoItem.js
@@ -16,14 +16,21 @@ export class TodoItem extends Component {
   };
 
   render() {
+    const { id, title } = this.props.todo;
     return (
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox" onChange={this.props.markComplete} />{' '}
-          {this.props.todo.title}
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{' '}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            x
+          </button>
         </p>
       </div>
-    );
+    ); 
   }
 }
 
@@ -32,8 +39,14 @@ TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
 };
 
-const itemStyle = {
-  backgroundColor: '#f4f4f4'
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 8px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
 };
 
 export default TodoItem;
